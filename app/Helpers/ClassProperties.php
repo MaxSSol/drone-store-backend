2510646f6d47a11a3b62460809cf949647e61dc7
Maksym Solomianyi <maxssolom@gmail.com>
2022-05-19 15:50:44 +0300

    Add helpers.

--- app/Helpers/ClassProperties.php
+++ app/Helpers/ClassProperties.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace app\Helpers;
+
+class ClassProperties
+{
+    public function getProperties($obj): array
+    {
+        $properties = [];
+        $reflection = new \ReflectionClass($obj);
+        $reflectionProperties = $reflection->getProperties();
+        foreach ($reflectionProperties as $property) {
+            $properties[] = $property->getName();
+        }
+        return $properties;
+    }
+}
